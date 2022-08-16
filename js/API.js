eb27ac82b29e7f94475cd5ac1f3ddd55b4aca61f
Onofrio Andrea Graziano <grazianoandrea33@gmail.com>
2022-08-16 02:39:04 +0200

    ff

--- js/API.js
+++ js/API.js
@@ -19,10 +19,15 @@ function launchApi(){
         })
         .then((data) => {
             console.log(data)
+            const date = new Date();
+            document.getElementById('date').innerText = 'Today, ' + date.getDate() + ' ' + date.toLocaleString('en-us', { month: 'short' });
+
             let generalData = {
                 city: city.charAt(0).toUpperCase() + city.slice(1),
                 temp: Math.round((data.daily[0].temp.max + data.daily[0].temp.min) / 2),
                 weather: data.daily[0].weather[0].main,
+                descr: data.daily[0].weather[0].description,
+                icon: data.daily[0].weather[0].icon,
                 wind: data.daily[0].wind_speed,
                 humidity: data.daily[0].humidity,
                 pressure: data.daily[0].pressure
@@ -52,7 +57,7 @@ function setGeneralData(data){
     document.getElementById('humidity').innerText = data.humidity + '%';
     document.getElementById('pressure').innerText = data.pressure + ' Pa';
 
-    
+    setImage(document.getElementById('general-weather-img'), data.weather, data.icon, data.descr);
 }
 
 function setHourlyData(data){
@@ -76,6 +81,10 @@ function addForecast(forecast, hour){
     const hourP = document.createElement('p');
     hourP.innerText = hour  + ':00';
 
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'weather icon');
+    setImage(img, forecast.weather[0].main, forecast.weather[0].icon, forecast.weather[0].description);
+
     const textDiv = document.createElement('div');
     textDiv.classList.add('text');
 
@@ -88,6 +97,7 @@ function addForecast(forecast, hour){
     textDiv.appendChild(degree);
     textDiv.appendChild(weather);
     carouselCell.appendChild(hourP);
+    carouselCell.appendChild(img);
     carouselCell.appendChild(textDiv);
 
     swiper.insert(carouselCell);
@@ -103,6 +113,38 @@ function removeForecast(){
     }
 }
 
-function setImage(element, weather){
-    if(weather == '')
+function setImage(element, weather, icon, description){
+    if(weather.toLowerCase() == 'clear'){
+        element.setAttribute('src', './assets/img/weather/sun.png');
+    }
+    else if(weather.toLowerCase() == 'clouds' && (icon == '02d' || icon == '02n' || icon == '03d' || icon == '03n')){
+        element.setAttribute('src', './assets/img/weather/sun-cloud.png');
+    }
+    else if(weather.toLowerCase() == 'clouds' && (icon != '02d' && icon != '02n')){
+        element.setAttribute('src', './assets/img/weather/cloudy.png');
+    }
+    else if(weather.toLowerCase() == 'rain' && (icon == '09d' || icon == '09n')){
+        element.setAttribute('src', './assets/img/weather/rain.png');
+    }
+    else if(weather.toLowerCase() == 'rain' && (icon != '09d' && icon != '09n')){
+        element.setAttribute('src', './assets/img/weather/sun-rain.png');
+    }
+    else if(weather.toLowerCase() == 'drizzle'){
+        element.setAttribute('src', './assets/img/weather/drizzle.png');
+    }
+    else if(weather.toLowerCase() == 'snow'){
+        element.setAttribute('src', './assets/img/weather/snow.png');
+    }
+    else if(weather.toLowerCase() == 'snow'){
+        element.setAttribute('src', './assets/img/weather/snow.png');
+    }
+    else if(weather.toLowerCase() == 'thunderstorm' && (description.includes('rain') || description.includes('drizzle'))){
+        element.setAttribute('src', './assets/img/weather/thunderstorm-rain.png');
+    }
+    else if(weather.toLowerCase() == 'thunderstorm' && (!description.includes('rain') && !description.includes('drizzle'))){
+        element.setAttribute('src', './assets/img/weather/thunderstorm.png');
+    }
+    else if(weather.toLowerCase() == 'snow'){
+        element.setAttribute('src', './assets/img/weather/snow.png');
+    }
 }
